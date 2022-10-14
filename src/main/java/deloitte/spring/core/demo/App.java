4f28d3d98f76516cd3e222e7bfabd70a43f6ed72
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-14 10:24:04 +0530

    spring core

--- src/main/java/deloitte/spring/core/demo/App.java
+++ src/main/java/deloitte/spring/core/demo/App.java
@@ -1,7 +1,38 @@
 package deloitte.spring.core.demo;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 
+ * @author Vaman 
+ * 
+ * Document Link: 
+ * 
+ * https://docs.google.com/document/d/1gjd0EmZKpZsIGuU7zTre9IZaLqHeqwjyQtUixNDbIVU/edit?usp=sharing 
+ *
+ */
+
+
 public class App {
+
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+
+		System.out.println("Start");
+
+//		Employee obj = new Employee(101, "Sonu", 90000);
+//		System.out.println(obj.toString());
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("SpringConfig.xml");
+
+		Employee obj2 = context.getBean(Employee.class);
+
+		System.out.println(obj2.toString());
+
+		((AbstractApplicationContext) context).close();
+
+		System.out.println("End");
+
 	}
 }
